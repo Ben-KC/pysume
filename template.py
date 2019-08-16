f1b5f3186a344d0d2d8e0cfd55a87357048dcfdc
Ben Kooiman-Cox <ben.cox@nucor.com>
2019-08-16 06:50:45 -0400

    Fix init and add opening/saving

--- template.py
+++ template.py
@@ -4,19 +4,38 @@ This module contains a class which can be invoked when creating a new resume,
 as well as classes for creating resume elements
 """
 
-def resume(dict):
-    def __init__(self, location="": str, basics={}: dict, work=[]: list,
-                 volunteer=[]: list, education=[]: list, awards=[]: list,
-                 publications=[]: list, skills=[]: list, languages=[]: list,
-                 interests=[]: list, references=[]: list):
-        self.super().__init__(['basics': basics,
+import collections
+import json
+
+
+class Resume(collections.UserDict):
+    def __init__(self, location: str="", basics: dict={}, work: list=[],
+                 volunteer: list=[], education: list=[], awards: list=[],
+                 publications: list=[], skills: list=[], languages: list=[],
+                 interests: list=[], references: list=[]):
+        super().__init__({'basics': basics,
                                'work': work, 
 			       'volunteer': volunteer,
-                               'education': eductation, 
+                               'education': education, 
                                'awards': awards,
                                'publications': publications, 
-                               'skills', skills,
+                               'skills': skills,
                                'languages': languages, 
                                'interests': interests,
-                               'references': references])
-	self.location = location
+                               'references': references})
+        self.location = location
+
+    categories = ["basics", "work", "volunteer", "education", "awards",
+                  "publications", "skills", "languages", "interests",
+                  "references"]
+    
+    def open_from_file(self, location: str):
+        with open(location, 'r') as res:
+            resume = json.load(res)
+
+        for category in self.categories:
+            self[category] = resume[category]
+
+    def save(self):
+        with open(self.location, 'w') as out:
+            out.write(json.JSONEncoder().encode(self.data))
